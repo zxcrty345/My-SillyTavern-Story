3d7e99c75145e462e6ce1b4c27567b441c4e9c0c
zxcrty345 <2154077375@qq.com>
2025-08-12 16:41:37 +0800

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@
 
 // ====================== 【全局配置区】 ======================
 const extensionName = "小剧场库";
-const extensionFolderPath = `scripts/extensions/third-party/My-Silly-Tavern-Stories`;
+const extensionFolderPath = `scripts/extensions/third-party/My-SillyTavern-Stories`;
 
 const SERVER_IP = "1.92.112.106"; 
 const SECRET_KEY = "qweasd123"; 
@@ -177,3 +177,4 @@ jQuery(async () => {
         console.error(`[${extensionName}] 加载 menu.html 或绑定事件时失败:`, error);
     }
 });
+
